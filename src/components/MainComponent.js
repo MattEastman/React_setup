f7ea00e74865f921f05aa29fb439413bfec2fc72
MattEastman <designeastman@gmail.com>
2019-01-21 00:54:49 -0800

    SPA intro

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent.js';
-import Footer from './FooterComponent';
+import Footer from './FooterComponent.js';
+import Home from './HomeComponent.js';
+import Contact from './ContactComponent.js';
+import {Switch, Route, Redirect} from 'react-router-dom';
+
+
 
 class Main extends Component {
 
@@ -11,20 +19,34 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
-
+    
   render() {
+      
+       const HomePage = () => {
+      return(
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
+      );
+    }
+      
     return (
       <div>
         <Header/>
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dishes={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+            <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+               <Route exact path='/contactus' component={Contact} />
+              <Redirect to="/home" />
+          </Switch>
        <Footer/>
       </div>
     );
