8a25f7e17249bb7ee02572a5557f48088ba0f78b
MattEastman <designeastman@gmail.com>
2019-02-04 04:21:44 -0800

    still need to fix dishdetail

---
+++ json-server/public/precache-manifest.d9de9c89f81d59d9d83e42796eeeee37.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "e16922c5fda968e008d6",
+    "url": "/static/css/main.3ccffd39.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "98966c1c7db85e64d3c1",
+    "url": "/static/js/1.98966c1c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e16922c5fda968e008d6",
+    "url": "/static/js/main.e16922c5.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "98966c1c7db85e64d3c1",
+    "url": "/static/css/1.24a1bc3e.chunk.css"
+  },
+  {
+    "revision": "a9294fde996ed8d6620955bc24fa0de4",
+    "url": "/index.html"
+  }
+];
